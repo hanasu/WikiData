ec1f446fb30b8f55cc970bb1ce7d0995ec4a404b
Brendan O'Dowd <bodowd@worcester.edu>
2016-03-11 13:33:41 -0500

    Additional formatting cleanup, preliminary clean text finished

--- Assignment2.py
+++ Assignment2.py
@@ -5,12 +5,22 @@ from nltk import word_tokenize
 
 f = codecs.open("smallcleaned.txt", encoding="UTF-8")
 contents = f.read()
+
+#the initial cleaning process seems to miss formatting between braces
+#this code will remove everything between two sets of braces
 contents = re.sub(r'\{\{(.*?)\}\}', "", contents)
+#catch the left over links that have no closing braces
+contents = re.sub(r'\{\{(.*)', "", contents)
+#remove the quotes that are left over, the filter 
+contents = re.sub(r'\'+', "", contents)
+#remove the filenames of images but retain the title text they are called from
+contents = re.sub(r'(.*)\|', "", contents)
+
 tokens = word_tokenize(contents)
 
 small = codecs.open('smalltokens.txt', 'w', 'utf8')
-tokens = filter(lambda word: word not in ',-.:;()', tokens)
+#punctuation is tokenized but they are not tokens, remove them
+tokens = filter(lambda word: word not in ',-.:;()%', tokens)
 for ele in tokens:
-    #ele.encode('utf-8').translate(None, string.punctuation)
     small.write(ele+'\n')
 small.close()
